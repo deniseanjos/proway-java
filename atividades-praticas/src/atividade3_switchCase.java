8bf6efb3b846c7526c42a15ce0d69f19f24783bc
Denise Anjos <deniseb.anjos@outlook.com>
2021-09-14 14:29:09 -0300

    atividade 2 - switch case

---
+++ atividades-praticas/src/atividade3_switchCase.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+public class atividade3_switchCase {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("=== CADASTRO DE FILMES, SERIES E DOCUMENTARIOS ====\nEscolha de acordo com menu para cadastro:\n1 - FILME\t|\t2 - SÉRIE\t|\t3 - DOCUMENTARIO\nDigite aqui: ");
+        int menu = sc.nextInt();
+
+        switch (menu){
+            case 1:
+                System.out.print("Informe o nome do filme: ");
+                String nomeFilme = sc.next();
+                System.out.print("Informe a descrição do filme: ");
+                String descricaoFilme = sc.next();
+                System.out.print("Informe o genero do filme: ");
+                String generoFime = sc.next();
+                System.out.print("Informe o ano de lançamento do filme: ");
+                int anoFilme = sc.nextInt();
+
+                System.out.printf("CADASTRO REALIZADO COM SUCESSO!\n" +
+                        "\tFILME: %s\n" +
+                        "\tDESCRIÇÃO: %s\n" +
+                        "\tGENERO: %s\n" +
+                        "\tANO DE LANÇAMENTO: %d", nomeFilme,descricaoFilme,generoFime,anoFilme);
+                break;
+            case 2:
+                System.out.print("Informe o nome da serie: ");
+                String nomeSerie = sc.next();
+                System.out.print("Informe a produtora da serie: ");
+                String produtoraSerie = sc.next();
+                System.out.print("Informe o ano de lançamento da serie: ");
+                int anoSerie = sc.nextInt();
+
+                System.out.printf("CADASTRO REALIZADO COM SUCESSO!\n" +
+                        "\tSERIE: %s\n" +
+                        "\tPRODUTORA: %s\n" +
+                        "\tANO DE LANÇAMENTO: %d", nomeSerie,produtoraSerie,anoSerie);
+                break;
+            case 3:
+                System.out.print("Informe o nome do documentario: ");
+                String nomeDocumentario = sc.next();
+                System.out.print("Informe o tema do documentario: ");
+                String temaDocumentario = sc.next();
+                System.out.print("Informe o ano de lançamento do documentario: ");
+                int anoDocumentario = sc.nextInt();
+
+                System.out.printf("CADASTRO REALIZADO COM SUCESSO!\n" +
+                        "\tDOCUMENTARIO: %s\n" +
+                        "\tTEMA: %s\n" +
+                        "\tANO DE LANÇAMENTO: %d", nomeDocumentario,temaDocumentario,anoDocumentario);
+                break;
+            default:
+                System.out.println("Opção inválida. ");
+                break;
+        }
+    }
+
+}
