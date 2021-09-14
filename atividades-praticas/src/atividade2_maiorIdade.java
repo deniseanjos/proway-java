3093e6ca3bddee85dd1d38b09b697d91e70dd477
Denise Anjos <deniseb.anjos@outlook.com>
2021-09-14 11:32:08 -0300

    atividade 2 - cadastro maior de idade

---
+++ atividades-praticas/src/atividade2_maiorIdade.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class atividade2_maiorIdade {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String nome, sobrenome;
+        int idade;
+
+        System.out.print("Informe sua idade: ");
+        idade = sc.nextInt();
+
+        if(idade >= 18) {
+            System.out.print("Digite seu nome: ");
+            nome = sc.next();
+
+            System.out.print("Digite seu sobrenome: ");
+            sobrenome = sc.next();
+
+            System.out.printf("CADASTRO REALIZADO COM SUCESSO!\nNOME COMPLETO: %s %s\nIDADE: %d", nome, sobrenome, idade);
+        } else {
+            System.out.println("Cadastro permitido apenas para maiores de idade.");
+        }
+    }
+}
