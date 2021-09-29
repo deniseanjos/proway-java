aee6b98a9c2b93763a13007be00d5f58dbbff23b
Denise Anjos <deniseb.anjos@outlook.com>
2021-09-29 14:36:18 -0300

    anotacoes

---
+++ atividades-praticas/src/anotacoes/variaveis_numerosFloat.java
@@ -0,0 +1,19 @@
+package anotacoes;
+
+public class variaveis_numerosFloat {
+
+    public static void main(String[] args) {
+        // 
+        System.out.println("---------- FLOAT ----------");
+        float minF = Float.MIN_VALUE;
+        float maxF = Float.MAX_VALUE;
+        System.out.println("MIN: " + minF + " | MAX: " + maxF);
+
+        //  
+        System.out.println("---------- DOUBLE ----------");
+        double minD = Double.MIN_VALUE;
+        double maxD = Double.MAX_VALUE;
+        System.out.println("MIN: " + minD + " | MAX: " + maxD);
+
+    }
+}
