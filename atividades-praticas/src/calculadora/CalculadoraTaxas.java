8bdfb680bcaf5799659d79d4cf0e6a0898c887ef
Denise Anjos <deniseb.anjos@outlook.com>
2021-09-30 12:09:34 -0300

    implementacao do metodo de calculo de taxa sobre saque e chamada no main

--- atividades-praticas/src/calculadora/CalculadoraTaxas.java
+++ atividades-praticas/src/calculadora/CalculadoraTaxas.java
@@ -1,10 +1,24 @@
 package calculadora;
 
 public class CalculadoraTaxas {
+
+    private int quantidadeSaques;
     
     public double taxa_transferencia(double valorTransferencia){
         double taxaCobrada = valorTransferencia * (0.001 / 100);
         return taxaCobrada;
+    }
+
+    public double taxa_saque(){
+        quantidadeSaques++;
+        double taxaSaque = 0.0;
+
+        if (quantidadeSaques % 5 == 0){
+            taxaSaque = 1.30;
+        }
+        
+        return taxaSaque;
+    
     }
     
 }
--- atividades-praticas/src/calculadora/Principal.java
+++ atividades-praticas/src/calculadora/Principal.java
@@ -3,6 +3,35 @@ package calculadora;
 public class Principal {
 
     public static void main(String[] args) {
+
+        CalculadoraTaxas c1 = new CalculadoraTaxas();
+
+        double taxaTransferencia = c1.taxa_transferencia(1500);
         
+        System.out.printf("%.4f", taxaTransferencia);
+
+        double saque1 = c1.taxa_saque();
+        double saque2 = c1.taxa_saque();
+        double saque3 = c1.taxa_saque();
+        double saque4 = c1.taxa_saque();
+        double saque5 = c1.taxa_saque();
+        double saque6 = c1.taxa_saque();
+        double saque7 = c1.taxa_saque();
+        double saque8 = c1.taxa_saque();
+        double saque9 = c1.taxa_saque();
+        double saque10 = c1.taxa_saque();
+        double saque11 = c1.taxa_saque();
+        
+        System.out.println(saque1);
+        System.out.println(saque2);
+        System.out.println(saque3);
+        System.out.println(saque4);
+        System.out.println(saque5);
+        System.out.println(saque6);
+        System.out.println(saque7);
+        System.out.println(saque8);
+        System.out.println(saque9);
+        System.out.println(saque10);
+        System.out.println(saque11);
     }
 }
