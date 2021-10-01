5bd6201509fecdc9757ab90f6711cdc1ba8007dc
Denise Anjos <deniseb.anjos@outlook.com>
2021-10-01 15:20:20 -0300

    criacao de variaveis e metodos na classe calculadora

--- atividades-praticas/src/impostos/Calculadora.java
+++ atividades-praticas/src/impostos/Calculadora.java
@@ -1,3 +1,20 @@
 public class Calculadora {
-    
+    private double impostoIss = 0.02;
+    private double impostoIof = 0.03;
+    private double impostoIr = 0.05;
+
+    public double calcular_iss(double valorEntrada){
+        double resultado = valorEntrada * impostoIss;
+        return resultado;
+    }
+
+    public double calcular_iof(double valorEntrada){
+        double resultado = valorEntrada * impostoIof;
+        return resultado;
+    }
+
+    public double calcular_ir(double valorEntrada){
+        double resultado = valorEntrada * impostoIr;
+        return resultado;
+    }
 }
