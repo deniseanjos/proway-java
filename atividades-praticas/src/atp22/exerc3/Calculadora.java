9c8b37053bcd1ed1d0b366b48766a1d4f2a2cf7c
Denise Anjos <deniseb.anjos@outlook.com>
2021-10-09 13:56:55 -0300

    exercicio 3 classe calculadora

---
+++ atividades-praticas/src/atp22/exerc3/Calculadora.java
@@ -0,0 +1,19 @@
+public class Calculadora {
+
+    public int soma(int a, int b){
+        return a + b;
+    }
+
+    public double soma(double a, double b){
+        return a + b;
+    }
+
+    public float soma(float a, float b, float c){
+        return a + b + c;
+    }
+
+    public long soma(long a, long b, long c){
+        return a + b + c;
+    }
+   
+}
