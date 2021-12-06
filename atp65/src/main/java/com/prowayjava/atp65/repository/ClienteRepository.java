b4ea02665ffec5330f6b02eeff59b2132b443a03
Denise Anjos <deniseb.anjos@outlook.com>
2021-12-06 11:11:20 -0300

    reorganizacao de pastas

---
+++ atp65/src/main/java/com/prowayjava/atp65/repository/ClienteRepository.java
@@ -0,0 +1,9 @@
+package com.prowayjava.atp65.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.prowayjava.atp65.model.Cliente;
+
+public interface ClienteRepository extends CrudRepository<Cliente, Integer>{
+
+}
